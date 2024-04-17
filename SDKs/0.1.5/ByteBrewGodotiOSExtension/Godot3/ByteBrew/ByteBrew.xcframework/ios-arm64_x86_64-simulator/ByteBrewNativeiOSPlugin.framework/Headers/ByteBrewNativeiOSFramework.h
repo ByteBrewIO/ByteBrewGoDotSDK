17546970236840dbc75f959f6f46c7c4691706e6
MaxWilk <max@bytebrew.io>
2024-04-17 14:33:03 -0400

    added new xcframework and incremented version

---
+++ SDKs/0.1.5/ByteBrewGodotiOSExtension/Godot3/ByteBrew/ByteBrew.xcframework/ios-arm64_x86_64-simulator/ByteBrewNativeiOSPlugin.framework/Headers/ByteBrewNativeiOSFramework.h
@@ -0,0 +1,18 @@
+//
+//  ByteBrewNativeiOSFramework.h
+//  ByteBrewNativeiOSFramework
+//
+//  Created by Cameron Hozouri on 4/8/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ByteBrewNativeiOSFramework.
+FOUNDATION_EXPORT double ByteBrewNativeiOSFrameworkVersionNumber;
+
+//! Project version string for ByteBrewNativeiOSFramework.
+FOUNDATION_EXPORT const unsigned char ByteBrewNativeiOSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ByteBrewNativeiOSFramework/PublicHeader.h>
+#import <ByteBrewNativeiOSPlugin/ByteBrewNativeiOSPlugin.h>
+
